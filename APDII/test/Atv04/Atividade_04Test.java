e91a349afdd88011ed1ec778d3ada2cc402fc7fc
thomazpicelli <thomazpicelli@gmail.com>
2015-03-25 20:08:21 -0300

    Entrega atividade 4

--- APDII/test/Atv04/Atividade_04Test.java
+++ APDII/test/Atv04/Atividade_04Test.java
@@ -11,14 +11,15 @@ import org.junit.Test;
 public class Atividade_04Test {
     @Test
     public void Test(){
-        Pessoa pessoa = new Pessoa("nome", "endereco");
+        Pessoa pessoa = new Pessoa("Joao", "Rua da Consolação");
         
         Tripulação tripulação = new Tripulação(2, pessoa);
         Passageiro passageiro = new Passageiro(6, pessoa);
         Agente agente = new Agente(1, pessoa);
         
-        assertEquals("nome", pessoa.getNome());
-        assertEquals("nome", agente.getPessoa().getNome());
-        assertEquals("endereco", passageiro.getPessoa().getEndereco());
+        assertEquals("Joao", pessoa.getNome());
+        assertEquals("Joao", agente.getPessoa().getNome());
+        assertEquals("Rua da Consolação", passageiro.getPessoa().getEndereco());
+        assertEquals("Rua da Consolação", tripulação.getPessoa().getEndereco());
     }
 }
